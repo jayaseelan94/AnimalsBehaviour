b123e329b8b6a95a8151b0655fb36fa34f969e07
User <User@DESKTOP-TDBNBN3>
2020-09-25 20:47:39 +0800

    Initial

---
+++ AnimalsBehaviour/src/main/java/com/animals/AnimalsBehaviourApplication.java
@@ -0,0 +1,13 @@
+package com.animals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsBehaviourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsBehaviourApplication.class, args);
+	}
+
+}
---
+++ AnimalsBehaviour/src/test/java/com/animals/AnimalsBehaviourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsBehaviourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
