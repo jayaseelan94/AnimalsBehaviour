451667c515bb8715475dff48f1202cdb0b8f4367
User <User@DESKTOP-TDBNBN3>
2020-09-25 21:00:19 +0800

    A. Let’s start with the basics
    1. Can you implement the sing() method for the bird?
        a. How did you unit test it?
        b. How did you optimize the code for maintainability?

---
+++ AnimalsBehaviour/src/main/java/com/animals/bean/Bird.java
@@ -0,0 +1,11 @@
+package com.animals.bean;
+
+public class Bird extends Animal{
+	public void fly() {
+        System.out.println("I am singing");
+    }
+
+    public void sing() {
+        System.out.println("I am singing");
+    }
+}
